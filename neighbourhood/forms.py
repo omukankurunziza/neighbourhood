a5e82b796e126060f700d6f552da3737e652c0b2
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-04-07 14:05:42 +0200

    add forms.py

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -4,11 +4,11 @@ from django.contrib.auth.forms import AuthenticationForm
 
 class ProfileForm(forms.ModelForm):
 	model = Profile
-	username = forms.CharField(label='Username',max_length = 30)
+	user = forms.CharField(label='Username',max_length = 30)
 	
 	bio = forms.CharField(label='Image Caption',max_length=500)
 	profile_image = forms.ImageField(label = 'Image Field')
-
+    # lacation = forms.TextField(label ='lacation',max_length =70)
 
 class ProfileUploadForm(forms.ModelForm):
 	class Meta:
