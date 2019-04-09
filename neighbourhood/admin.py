9ccf80e99222a3c065c9aeb8b5bab988d5ecdd62
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-04-09 22:48:17 +0200

    add admin.py

--- neighbourhood/admin.py
+++ neighbourhood/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Admin,Profile,Neighbourhood,Business,User
+
 # Register your models here.
+
+admin.site.register(Admin)
+admin.site.register(Profile)
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
